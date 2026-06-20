c1031156496ee4cede42cfedaec20b57829aafc0
PandinhaTV <57328243+PandinhaTV@users.noreply.github.com>
2026-06-20 16:21:33 +0100

    update

--- scripts/Hud.cs
+++ scripts/Hud.cs
@@ -6,4 +6,5 @@ public partial class Hud : Node2D
     
     [Export]public Label score;
     [Export]public Label life;
+    
 }
---
+++ scripts/MainGame.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class MainGame : Node2D
+{
+    [Export] public Hud hud;
+    public override void _Ready()
+    {
+        Label lifeValue = hud.life;
+        Label scoreValue = hud.score;
+    }
+}
