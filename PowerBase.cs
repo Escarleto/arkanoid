570f8c59dd13dfb69d3e4f5a0faadeca339a4542
PandinhaTV <57328243+PandinhaTV@users.noreply.github.com>
2026-06-20 18:32:52 +0100

    update power

---
+++ PowerBase.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+public enum PowerUp { Laser, Enlarge, Catch, Slow, Break, Disruption, Player } 
+public partial class PowerBase : Node2D
+{
+    [Export] public Texture2D[] StateTextures; // indexed to match enum order
+    [Export] public PowerUp CurrentState = PowerUp.Laser;
+    
+    private void ApplyState()
+    {
+        _textureRect.Texture = StateTextures[(int)CurrentState];
+    }
+}
