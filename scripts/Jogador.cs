2e9f2924ebc354daddb3266245b3fa679398c3e0
Escarleto <lucascliberato31@gmail.com>
2026-06-20 16:24:45 +0100

    bola

--- scripts/Jogador.cs
+++ scripts/Jogador.cs
@@ -6,10 +6,11 @@ public partial class Jogador : CharacterBody2D
 	private int Vida = 2;
 	private float H_Direction = 0;
 	[Export] private float Speed = 1f;
+	[Export] private float SpeedFallofRate = 1f;
 
 	public override void _PhysicsProcess(double delta)
 	{
-		H_Direction = Input.GetAxis("ui_left", "ui_right");
+		H_Direction = Mathf.MoveToward(H_Direction, Input.GetAxis("ui_left", "ui_right"), SpeedFallofRate * (float)delta);
 
 		Velocity = new Vector2(H_Direction * Speed, 0f);
 		MoveAndSlide();
